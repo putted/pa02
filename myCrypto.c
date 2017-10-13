d9dbee6998bfe5bd08a245b340b2ee42a2273fb5
Matthew Jennings <jenninmx@dukes.jmu.edu>
2017-10-12 22:43:55 -0400

    myCrypto.c updated

--- myCrypto.c
+++ myCrypto.c
@@ -20,17 +20,40 @@ void handleErrors(char *msg) {
 #define INPUT_CHUNK 16384
 
 size_t fileDigest(int fd_in, uint8_t *digest, int fd_save) {
-  /*
-  Read all the incoming data from 'fd_in' file descriptor
-  Compute the SHA256 hash value of this incoming data into the array 'digest'
-  If the file descriptor 'fd_save' is > 0, store a copy of the incoming data to 'fd_save'
-  Returns actual size in bytes of the computed hash value
-  */
-  unsigned int mdLen;
+	
+  // Declare mdCtx and buffer to be used	
+  EVP_MD_CTX* mdCtx;
+  char *buff[INPUT_CHUNK];
+  
+  if((mdCtx = EVP_MD_CTX_create()) == NULL)
+	handleErrors("Failed to create mdCtx.");
 
-  //MISSING CODE
+  if (1 != EVP_DigestInit_ex(mdCtx, EVP_sha256(), NULL))
+	handleErrors("Failed to initialize mdCtx.");
+  
+  // Read all the incoming data from 'fd_in' file descriptor
+  int iter; 
+  while (iter = read(fd_in, buff, INPUT_CHUNK), iter != 0)
+  {
+  // Compute the SHA256 hash value of this incoming data into the array 'digest'	  
+	if (1 != EVP_DigestUpdate(mdCtx, buff, iter));
+	  handleErrors("Failed to update digest.");  
+  }
+	
+  int digest_len = strlen(digest);
+  if (1 != EVP_DigestFinal_ex(mdCtx, digest, &digest_len))
+    handleErrors("Failed to finalize digest.");
 
-  return mdLen;
+  EVP_MD_CTX_destroy(mdCtx);
+  
+  // If the file descriptor 'fd_save' is > 0, store a copy of the incoming data to 'fd_save'
+  if (fd_save > 0)
+  {
+	  fd_save = fd_in;
+  }
+  
+  // Returns actual size in bytes of the computed hash value
+  return digest_len;
 }
 
 RSA *getRSAfromFile(char *filename, int public) {
